ea040fedd5a95bd769ba63e92e6863e2cf64e19f
vmonteco <vmonteco@student.42.fr>
2018-02-22 20:59:23 +0100

    updates

--- 01_dockerfiles/ex03/docker-entrypoint.sh
+++ 01_dockerfiles/ex03/docker-entrypoint.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
 # Steps to add :
-# 1- Update DB conf
+# 1- Update DB conf (config/database.yml)
+#    - database name
+#    - username
+#    - password # REQUIRED
+#    - host     # REQUIRED
 # 2- Update redis server conf
-# 3- Update localhost
+# 3- Update host in gitlab.yml
+# 4- Update email_from in gitlab.yml (use host value)
+# 5- Update email_reply_to
+# 6- Install gitlab-shell (export `RUBYOPT=--disable-gems` and compile ruby with `configure --disable-rubygems`)
 
 bash

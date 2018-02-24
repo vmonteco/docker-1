d88a9a48c7f19f817ade9ed6a0a883364ba1ded6
Vincent MONTECOT <vmonteco@e2r9p11.42.fr>
2018-02-24 21:31:50 +0100

    updates

--- 01_dockerfiles/ex03/docker-entrypoint.sh
+++ 01_dockerfiles/ex03/docker-entrypoint.sh
@@ -1,3 +1,25 @@
 #!/usr/bin/env bash
 
-bash -c "sudo service redis-server restart && sudo service postgresql restart && service nginx start && service gitlab start" | tail
+function entrypoint() {
+	if [ -z ${SSH_PORT} ]
+	then
+	   sed -i "/    # ssh_port: 22/c\    ssh_port: $SSH_PORT" /home/git/gitlab/config/gitlab.yml
+	fi
+	if [ -z ${GITLAB_HOST} ]
+	then
+	   sed -i "/    host: localhost/c\    host: $GITLAB_HOST" /home/git/gitlab/config/gitlab.yml
+	fi
+	service gitlab start &
+	service redis-server restart
+	service postgresql restart
+	service ssh restart
+	nginx -g "daemon off;" | tail
+}
+
+if [ $# -eq 0 ]
+then
+	entrypoint
+else
+	bash $#
+fi
+

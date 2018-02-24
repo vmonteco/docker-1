a3acf7643fd69ae5ca6a4de7d9259922cb57ab65
vmonteco <vmonteco@student.42.fr>
2018-02-24 16:59:51 +0100

    ex03 seems to work a bit

--- 01_dockerfiles/ex03/docker-entrypoint.sh
+++ 01_dockerfiles/ex03/docker-entrypoint.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-bash -c "sudo service redis-server restart && sudo service postgresql restart && service nginx start && gitlab-ctl start" | tail
+bash -c "sudo service redis-server restart && sudo service postgresql restart && service nginx start && service gitlab start" | tail
